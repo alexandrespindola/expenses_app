9e6fcc1d89381b5594581222b74304413fb04a63
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-05-22 10:06:31 +0200

    feat: Add transaction form and list components
    The code changes include adding the `TransactionForm` and `TransactionList` components to the project. These components are responsible for rendering the form to add new transactions and displaying a list of existing transactions, respectively.
    
    The commit message follows the conventional format of starting with a type (`feat` for a new feature) and providing a concise and descriptive message of the changes made.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:expenses/components/transaction_form.dart';
 import 'package:flutter/material.dart';
-import 'components/transaction_user.dart';
+import './components/transaction_list.dart';
+import '../models/transaction.module.dart';
 
 main() => runApp(const ExpensesApp());
 
@@ -8,36 +10,97 @@ class ExpensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyHomePage());
+    return MaterialApp(
+      home: MyHomePage(),
+    );
   }
 }
 
-// ignore: must_be_immutable, use_key_in_widget_constructors
-class MyHomePage extends StatelessWidget {
-  // MyHomePage({super.key, this.titleController, this.valueController});
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo tênis de corrida Nike',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: DateTime.now().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return TransactionForm(_addTransaction);
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: const Text('Despesas pessoais'), actions: const <Widget>[
-              IconButton(onPressed: null, icon: Icon(Icons.abc))
-            ]),
-        body: const SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(
-                width: double.infinity,
-                child: Card(
-                  color: Colors.blue,
-                  elevation: 5,
-                  child: Text('Gráfico'),
-                ),
+      appBar: AppBar(
+        title: const Text('Despesas pessoais'),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () => _openTransactionFormModal(context),
+            icon: const Icon(Icons.add),
+            color: Colors.white,
+            highlightColor: Colors.purple.shade200,
+            tooltip: 'Add new transaction',
+          )
+        ],
+        backgroundColor: Colors.purple.shade800,
+        centerTitle: true,
+        foregroundColor: Colors.white,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                elevation: 5,
+                child: Text('Gráfico'),
               ),
-              TransactionUser()
-            ],
-          ),
-        ));
+            ),
+            TransactionList(_transactions),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _openTransactionFormModal(context),
+        backgroundColor: Colors.purple.shade300,
+        splashColor: Colors.purple.shade700,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(50.0))),
+        child: const Icon(Icons.add, color: Colors.white),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
 }
