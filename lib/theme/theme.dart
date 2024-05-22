58ac1dbaed9e8328d4c236cdf0517d9fddc8d1d2
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-05-22 12:12:26 +0200

    chore: Update theme colors and fonts
    The code changes include updating the theme colors and fonts in the `theme.dart` file. The primary and secondary colors have been changed to green and amber, respectively. The text theme has been updated to use the 'Nunito' font family. Additionally, the dark theme has been modified to compute the opposite colors for a better contrast. The `pubspec.yaml` file has also been updated to include the 'Nunito' font family with its corresponding font assets.
    
    This commit message follows the conventional format of starting with a type (`chore` for a chore or maintenance task) and providing a concise and descriptive message of the changes made.

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,29 +1,32 @@
 import 'package:flutter/material.dart';
 
-  Color primary = Colors.green;
-  Color secondary = Colors.amber;
+Color primary = Colors.green;
+Color secondary = Colors.amber;
 
 ThemeData lightTheme() {
-  
   final ThemeData base = ThemeData.light();
   return base.copyWith(
     colorScheme: base.colorScheme.copyWith(
       primary: primary,
       secondary: secondary,
     ),
-    textTheme: base.textTheme.copyWith(
-      titleLarge: base.textTheme.titleLarge!.copyWith(
-        fontFamily: 'Arial',
-        fontSize: 20,
-        fontWeight: FontWeight.bold,
-        color: Colors.black,
-      ),
-    ),
+    textTheme: base.textTheme
+        .apply(
+          fontFamily: 'Nunito',
+        )
+        .copyWith(
+          titleLarge: base.textTheme.titleLarge!.copyWith(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
     floatingActionButtonTheme: base.floatingActionButtonTheme.copyWith(
       backgroundColor: secondary,
     ),
     appBarTheme: const AppBarTheme(
       titleTextStyle: TextStyle(
+        fontFamily: 'Nunito',
         fontWeight: FontWeight.bold,
       ),
     ),
@@ -31,31 +34,37 @@ ThemeData lightTheme() {
 }
 
 ThemeData darkTheme() {
-  
   // Compute "opposite" colors for dark theme
-  Color darkPrimary = HSLColor.fromColor(primary).withLightness(1 - primary.computeLuminance()).toColor();
-  Color darkSecondary = HSLColor.fromColor(secondary).withLightness(1 - secondary.computeLuminance()).toColor();
-  
+  Color darkPrimary = HSLColor.fromColor(primary)
+      .withLightness(1 - primary.computeLuminance())
+      .toColor();
+  Color darkSecondary = HSLColor.fromColor(secondary)
+      .withLightness(1 - secondary.computeLuminance())
+      .toColor();
+
   final ThemeData base = ThemeData.dark();
   return base.copyWith(
     colorScheme: base.colorScheme.copyWith(
       primary: darkPrimary,
       secondary: darkSecondary,
     ),
-    textTheme: base.textTheme.copyWith(
-      titleLarge: base.textTheme.titleLarge!.copyWith(
-        fontFamily: 'Ubuntu',
-        fontSize: 20,
-        fontWeight: FontWeight.bold,
-        color: Colors.white,
-      ),
-    ),
+    textTheme: base.textTheme
+        .apply(
+          fontFamily: 'Nunito',
+        )
+        .copyWith(
+          titleLarge: base.textTheme.titleLarge!.copyWith(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
     floatingActionButtonTheme: base.floatingActionButtonTheme.copyWith(
       backgroundColor: darkSecondary,
     ),
     appBarTheme: const AppBarTheme(
       titleTextStyle: TextStyle(
-        fontFamily: 'Ubuntu',
+        fontFamily: 'Nunito',
         fontSize: 20,
         fontWeight: FontWeight.bold,
       ),
