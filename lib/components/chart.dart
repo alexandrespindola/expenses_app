d88323659861b22260f5d5e1b89ae852a7ed3c55
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-05-29 06:45:59 +0200

    Refactor Chart component to use groupedTransactions for rendering
    The code changes in the Chart component refactor the way transactions are rendered. Instead of manually printing the day and value for each transaction, the component now uses the groupedTransactions list to dynamically render the data. This improves code readability and maintainability.
    
    This commit message follows the conventional format of starting with a type (`refactor` for code refactoring) and providing a concise and descriptive message of the changes made.

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -25,9 +25,6 @@ class Chart extends StatelessWidget {
         }
       }
 
-      print(DateFormat.E().format(weekDay)[0]);
-      print(totalSum);
-
       return {
         'day': DateFormat.E().format(weekDay)[0],
         'value': totalSum,
@@ -38,11 +35,14 @@ class Chart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     groupedTransactions;
-    return const Card(
-        elevation: 6,
-        margin: EdgeInsets.all(20),
-        child: Row(
-          children: <Widget>[],
-        ));
+    return Card(
+      elevation: 6,
+      margin: const EdgeInsets.all(20),
+      child: Row(
+        children: groupedTransactions.map((tr) {
+          return Text('${tr['day']}: ${tr['value']}');
+        }).toList(),
+      ),
+    );
   }
 }
